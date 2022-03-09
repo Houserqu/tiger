80f3ac5f64f5759d8853f9faaa9ce0abc058f17b
houserqu <houserqu@tencent.com>
2022-03-09 15:49:20 +0800

    feat: 前端切换到 vite + react + ts 技术栈

--- admin/config-overrides.js
+++
@@ -1,8 +0,0 @@
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  config.plugins.push(new MonacoWebpackPlugin())
-  // config.devtool = 'inline-source-map';
-  return config;
-}
---
+++ admin/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: '../public',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3001,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
+  }
+})
