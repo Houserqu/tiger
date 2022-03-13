66a3650ac80e39c72eb9686cd5fad47ebf6bdbd3
houserqu <houserqu@tencent.com>
2022-03-13 22:17:19 +0800

    feat: 页面 layout

--- admin/src/layout/AdminLayout.tsx
+++ admin/src/layout/AdminLayout.tsx
@@ -1,9 +1,59 @@
+import { Avatar } from "amis";
 import { Outlet } from "react-router-dom";
+import renderAmis from "../util/renderAmis";
 
-export default function AdminLayout({children}: any) {
+export default function AdminLayout({ children }: any) {
   return (
     <div>
-      <Outlet />
+      <div
+        className="bg-white flex justify-between px-4 items-center"
+        style={{ height: '60px', borderBottom: 'solid 1px rgb(231 231 231)'}}
+      >
+        <div>
+          <i className='fab fa-html5 text-2xl' />
+        </div>
+        <Avatar icon='fa fa-user'/>
+      </div>
+      <div style={{ height: 'calc(100vh - 60px)', display: 'flex' }}>
+        <div className='h-full bg-bule-100 bg-white'>
+          {renderAmis({
+            "type": "page",
+            "body": {
+              "type": "nav",
+              "stacked": true,
+              "className": "w-md",
+              "links": [
+                {
+                  "label": "Nav 1",
+                  "to": "/docs/index",
+                  "icon": "fa fa-user",
+                  "active": true
+                },
+                {
+                  "label": "Nav 2",
+                  "unfolded": true,
+                  "children": [
+                    {
+                      "label": "Nav 2-1",
+                    },
+                    {
+                      "label": "Nav 2-2",
+                      "to": "/docs/api-2-2"
+                    }
+                  ]
+                },
+                {
+                  "label": "Nav 3",
+                  "to": "/docs/renderers"
+                }
+              ]
+            }
+          })}
+        </div>
+        <div className="w-full bg-gray-100">
+          <Outlet />
+        </div>
+      </div>
     </div>
   )
 }
--- admin/src/page/Dashboard/Dashboard.tsx
+++ admin/src/page/Dashboard/Dashboard.tsx
@@ -1,3 +1,17 @@
-export default function Dashboard({children}: any) {
-  return <div>dashboard</div>
+import { Button } from "amis";
+import renderAmis from "../../util/renderAmis";
+
+export default function Dashboard({ children }: any) {
+  return (
+    <div>
+      {renderAmis({
+        "type": "page",
+        "body": {
+          "type": "panel",
+          "title": "首页",
+          "body": "首页"
+        }
+      })}
+    </div>
+  )
 }
