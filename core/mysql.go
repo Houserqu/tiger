6b5a34008de174268d08ab05ef6d2a5070d55ab3
houserqu <houserqu@tencent.com>
2022-05-19 13:15:44 +0800

    feat: 优化日志输出

--- core/mysql.go
+++ core/mysql.go
@@ -3,7 +3,6 @@ package core
 import (
 	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"github.com/spf13/viper"
@@ -16,7 +15,7 @@ var Mysql *gorm.DB
 
 func InitMysql() {
 	dbLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+		Logger, // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
 		logger.Config{
 			SlowThreshold:             time.Second, // 慢 SQL 阈值
 			LogLevel:                  logger.Info, // 日志级别
